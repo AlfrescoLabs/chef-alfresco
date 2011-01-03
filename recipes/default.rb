2d307b97a729d59534143b847a1927b2be1eeee0
fnichol <fnichol@nichol.ca>
2011-01-03 01:31:14 -0700

    Start adding recipe pre-requisites.

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,3 +16,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+include_recipe "java"
+include_recipe "openoffice::headless"
+include_recipe "imagemagick"
+include_recipe "tomcat"
