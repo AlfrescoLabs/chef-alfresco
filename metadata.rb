f886b33b7410e243c832e44b90ad5ce29d5e3865
Maurizio Pillitu <maurizio.pillitu@alfresco.com>
2014-04-11 14:30:26 +0200

    added cookbook name

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,10 @@
+name             "alfresco"
 maintainer       "Maurizio Pillitu"
 maintainer_email ""
 license          "Apache 2.0"
 description      "Installs Alfresco Community and Enterprise Edition."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.2.0"
-
-supports "ubuntu"
 
 depends "artifact-deployer"
 depends "imagemagick"
