5b31535dcbec02f2a75a691ff7a4c7744bec9ce7
Maurizio Pillitu <maoo@apache.org>
2015-05-13 17:22:43 +0200

    forcing to use jdk

--- recipes/_attributes.rb
+++ recipes/_attributes.rb
@@ -5,6 +5,7 @@ node.default["java"]["default"]                                 = true
 node.default["java"]["accept_license_agreement"]                = true
 node.default["java"]["install_flavor"]                          = "oracle"
 node.default["java"]["jdk_version"]                             = "7"
+node.default["java"]["java_home"] = "/usr/lib/jvm/java"
 node.default["java"]["oracle"]['accept_oracle_download_terms']  = true
 
 ##########################
