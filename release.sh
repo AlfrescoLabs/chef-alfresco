ac2cf30818d940da159ed77746938e8fa9d0f28d
Maurizio Pillitu <maoo@apache.org>
2015-06-08 17:48:32 +0200

    Create release.sh

---
+++ release.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# Script for releasing a tar.gz berkshelf artifact into Alfresco Nexus Internal
+
+function buildArtifact () {
+  /opt/chefdk/embedded/bin/rake
+}
+
+function getCurrentVersion () {
+  version=`cat metadata.rb| grep version|awk '{print $2}'`
+  echo ${version:1:5}
+}
+
+function getIncrementedVersion () {
+  version=$(getCurrentVersion)
+  echo $version | awk -F'[.]' '{print $1,".",$2,".",$3+1}' | tr -d '[[:space:]]'
+}
+
+function incrementVersion () {
+  export currentVersion=$(getCurrentVersion)
+  export nextVersion=$(getIncrementedVersion)
+
+  sed "s/$currentVersion/$nextVersion/" metadata.rb > metadata.rb.tmp
+  rm -f metadata.rb
+  mv metadata.rb.tmp metadata.rb
+}
+
+function deploy () {
+  repo_name="internal-releases"
+  if [[ $1 == *SNAPSHOT ]]; then
+    repo_name="internal-snapshots"
+  fi
+
+  echo "Running: mvn deploy:deploy-file -Dfile=$(echo *.tar.gz) -DrepositoryId=alfresco-internal -Durl=https://artifacts.alfresco.com/nexus/content/repositories/$repo_name -DgroupId=org.alfresco.devops -DartifactId=img-basic -Dversion=$1 -Dpackaging=tar.gz"
+
+  mvn deploy:deploy-file -Dfile=$(echo *.tar.gz) -DrepositoryId=alfresco-internal -Durl=https://artifacts.alfresco.com/nexus/content/repositories/$repo_name -DgroupId=org.alfresco.devops -DartifactId=img-basic -Dversion=$1 -Dpackaging=tar.gz
+}
+
+function deploySnapshot () {
+  buildArtifact
+  current_version=$(getCurrentVersion)
+  deploy "$current_version-SNAPSHOT"
+}
+
+function release () {
+  buildArtifact
+  deploy $(getCurrentVersion)
+  incrementVersion
+}
+
+# Done by Bamboo
+# export HOME=/tmp
+export PATH=/opt/chefdk/embedded/bin/:$PATH
+export PATH=/opt/apache-maven/bin:$PATH
+
+MODE=$1
+
+if [ "$MODE" == "snapshot" ]; then
+  deploySnapshot
+elif [ "$MODE" == "nextversion" ]; then
+  getIncrementedVersion
+elif [ "$MODE" == "thisversion" ]; then
+  getCurrentVersion
+else
+  release
+fi
