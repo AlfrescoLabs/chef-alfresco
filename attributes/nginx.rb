6e8456c0ee4d6ca0d757b9bf8cd3ce626169d246
Maurizio (maoo) Pillitu <maurizio.pillitu@alfresco.com>
2015-06-27 00:54:36 +0200

    fixing nginx overrides

--- attributes/nginx.rb
+++ attributes/nginx.rb
@@ -10,3 +10,34 @@ default['nginx']['resolver'] = "8.8.4.4 8.8.8.8"
 default['nginx']['port'] = "80"
 
 default['nginx']['proxy_port'] = node['haproxy']['port']
+
+default['nginx']['proxy_set_header_port'] = node['nginx']['port']
+
+default['nginx']['config'] = [
+  "user  nobody;",
+  "worker_processes  2;",
+  "events {",
+  "    worker_connections  1024;",
+  "}",
+  "http {",
+  "    sendfile on;",
+  "    tcp_nopush on;",
+  "    tcp_nodelay on;",
+  "    keepalive_timeout 65;",
+  "    types_hash_max_size 2048;",
+  "    include /etc/nginx/mime.types;",
+  "    default_type application/octet-stream;",
+  "    access_log /var/log/nginx/access.log;",
+  "    error_log /var/log/nginx/error.log;",
+  "    gzip on;",
+  "    gzip_disable \"msie6\";",
+  "    server {",
+  "        listen #{node['nginx']['port']} default_server;",
+  "        listen [::]:80 default_server;",
+  "        server_name #{node['alfresco']['default_hostname']};",
+  "        location / {",
+  "            proxy_set_header Host $host:#{node['nginx']['proxy_set_header_port']};",
+  "            proxy_pass http://localhost:#{node['nginx']['proxy_port']};",
+  "        }",
+  "    }",
+  "}"]
