e995b5ebef0cafdbd0155f7b1171e9f3eff6e5ae
Maurizio Pillitu <maoo@apache.org>
2015-06-29 01:55:54 +0200

    Update nginx.rb

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -9,9 +9,9 @@ file "/etc/nginx/conf.d/default.conf" do
   action :delete
 end
 
-# nginx::packages must be explicitely called, to install nginx yum repos
+# nginx::repo must be explicitely called, to install nginx yum repos
 # and get the latest package versions
-include_recipe 'nginx::packages'
+include_recipe 'nginx::repo'
 
 include_recipe 'nginx::default'
 
