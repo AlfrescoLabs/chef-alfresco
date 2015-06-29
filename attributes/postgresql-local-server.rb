a6f0b1b62b925dc0e26f1fceeec7264ba8737f5e
Maurizio Pillitu <maoo@apache.org>
2015-06-30 01:41:09 +0200

    Update postgresql-local-server.rb

--- attributes/postgresql-local-server.rb
+++ attributes/postgresql-local-server.rb
@@ -3,7 +3,4 @@
 # default['rsyslog']['file_inputs']['psql-error']['severity'] = 'error'
 # default['rsyslog']['file_inputs']['psql-error']['priority'] = 57
 
-default['alfresco']['properties']['db.port'] = '5432'
-default['alfresco']['properties']['db.params'] = ''
-
 default['postgresql']['version'] = "9.3"
