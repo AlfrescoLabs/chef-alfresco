9feeba9851539bcf35ae3b175c5e29cf83f7f93d
alfresco-build <info@alfresco.com>
2015-06-30 12:59:25 +0100

    Bumping version to v0.6.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email ""
 license          "Apache 2.0"
 description      "Installs Alfresco Community and Enterprise Edition."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version "0.6.1"
+version "0.6.2"
 
 # Resolved by Berkshelf, not present in Supermarket or forked
 depends "file"
@@ -27,7 +27,7 @@ depends "nginx", "~> 2.7.6"
 depends "openssl", "~> 4.0.0"
 depends 'rsyslog', "~> 1.15.0"
 depends "swftools", "~> 0.2.4"
-depends 'yum-epel', '~> 0.6.1'
+depends 'yum-epel', '~> 0.6.2'
 depends 'yum-repoforge', '~> 0.5.1'
 depends 'yum-atrpms', '~> 0.1.0'
 
