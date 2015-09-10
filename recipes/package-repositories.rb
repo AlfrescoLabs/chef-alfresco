5d2f18c0d59ed6ace3ea009da5713c112d3ba779
Maurizio (maoo) Pillitu <maurizio.pillitu@alfresco.com>
2015-09-10 12:10:53 +0200

    using mirror for atrpms

--- recipes/package-repositories.rb
+++ recipes/package-repositories.rb
@@ -1,3 +1,6 @@
+# Fixing temporary downtime of atrpms
+node.set['yum']['atrpms']['baseurl'] = "https://www.mirrorservice.org/sites/dl.atrpms.net/el$releasever-$basearch/atrpms/stable"
+
 if node['platform_family'] == "rhel"
   include_recipe 'yum-epel::default'
   include_recipe 'yum-repoforge::default'
