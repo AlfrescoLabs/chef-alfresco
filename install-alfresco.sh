4695c65cef8b8bb4ac89815438fedfb5f691cbec
Maurizio (maoo) Pillitu <maurizio.pillitu@alfresco.com>
2015-10-14 12:37:12 +0200

    more code on install-alfresco.sh and chef pre-configured nodes

--- install-alfresco.sh
+++ install-alfresco.sh
@@ -1,14 +1,32 @@
+#NODE_NAME=share
+#NODE_NAME=solr
+
+if [ -z "$NODE_NAME" ]; then
+  NODE_NAME=allinone
+fi
+
+if [ -z "$NODE_URL" ]; then
+  NODE_URL=https://raw.githubusercontent.com/Alfresco/chef-alfresco/master/nodes/$NODE_NAME.json
+fi
+
+if [ -z "$COOKBOOKS_TARBALL_URL" ]; then
+  COOKBOOKS_TARBALL_URL=https://artifacts.alfresco.com/nexus/service/local/repositories/releases/content/org/alfresco/devops/chef-alfresco/0.6.6/chef-alfresco-0.6.6.tar.gz
+fi
+
 # Download chef-alfresco tar.gz into /tmp folder
-curl -L https://artifacts.alfresco.com/nexus/service/local/repositories/releases/content/org/alfresco/devops/chef-alfresco/0.6.6/chef-alfresco-0.6.6.tar.gz > /tmp/chef-alfresco-0.6.6.tar.gz
+curl -L $COOKBOOKS_TARBALL_URL > /tmp/chef-alfresco.tar.gz
 
 # Unpack it in /tmp
-tar xvzf /tmp/chef-alfresco-0.6.6.tar.gz -C /tmp/chef-alfresco
+tar xvzf /tmp/chef-alfresco.tar.gz -C /tmp/chef-alfresco
 
 # Copy cookbooks into /etc/chef location
 cp -rf /tmp/chef-alfresco/cookbooks /etc/chef
 
 # Download Chef JSON attribute
-curl -L https://artifacts.alfresco.com/nexus/service/local/repositories/releases/content/org/alfresco/devops/chef-alfresco/0.6.6/chef-alfresco-0.6.6.tar.gz > /etc/chef/attributes.json
+curl -L $NODE_URL > /etc/chef/attributes.json
+
+# TODO - Apply placeholding for @@values@@
+# ...
 
 # Run chef
 cd /etc/chef
