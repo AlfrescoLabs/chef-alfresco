8b4d5f3d5967861600a04c283a7ea907e8fa0f5c
Maurizio (maoo) Pillitu <maurizio.pillitu@alfresco.com>
2015-10-19 15:46:47 +0200

    fixed sudo run for cert creation

--- recipes/_certs.rb
+++ recipes/_certs.rb
@@ -28,14 +28,14 @@ rescue
   ssl_dhparam_file = "#{ssl_folder}/#{ssl_fqdn}.dhparam"
 
   execute "create-fake-ssl-keypair" do
-    command "sudo openssl req -subj '/C=UK/ST=Berkshire/L=Maidenhead/O=Alfresco/CN=#{ssl_fqdn}' -x509 -days 3650 -batch -nodes -newkey rsa:4096 -keyout #{ssl_key_file} -out #{ssl_crt_file}"
+    command "openssl req -subj '/C=UK/ST=Berkshire/L=Maidenhead/O=Alfresco/CN=#{ssl_fqdn}' -x509 -days 3650 -batch -nodes -newkey rsa:4096 -keyout #{ssl_key_file} -out #{ssl_crt_file}"
     not_if "test -f #{ssl_key_file}"
   end
 
   # TODO - .chain and .nginx are the same; use only .chain
   # TODO - ssh_trust_file should be created and concatenated to .chain file
   execute "create-chain-file" do
-    command " echo '\n' >> #{ssl_crt_file}; cat #{ssl_crt_file} #{ssl_key_file} > #{ssl_chain_file}"
+    command "echo '\n' >> #{ssl_crt_file}; cat #{ssl_crt_file} #{ssl_key_file} > #{ssl_chain_file}"
     not_if "test -f #{ssl_chain_file}"
   end
   execute "create-nginxcrt-file" do
