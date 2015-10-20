6d19efe53537ce70fdd34ca3494afaa944902259
Maurizio (maoo) Pillitu <maurizio.pillitu@alfresco.com>
2015-10-20 09:57:31 +0200

    using fixed name for cert files

--- attributes/_certs.rb
+++ attributes/_certs.rb
@@ -1,2 +1,2 @@
-default['alfresco']['certs']['ssl_fqdn'] = node['alfresco']['public_hostname']
+default['alfresco']['certs']['ssl_fqdn'] = 'alfresco'
 default['alfresco']['certs']['ssl_folder'] = "/etc/pki/tls/certs"
