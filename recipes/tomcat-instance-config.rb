70586effdb4a3b87f99db9819d051cdc8892c739
Maurizio (maoo) Pillitu <maurizio.pillitu@alfresco.com>
2015-10-23 10:44:49 +0200

    fixed syntax issue... we still need to enforce these checks on Bamboo!

--- recipes/tomcat-instance-config.rb
+++ recipes/tomcat-instance-config.rb
@@ -1,7 +1,7 @@
 global_templates = node['tomcat']['global_templates']
 if global_templates
   global_templates.each do |global_template|
-    irectory global_template['dest'] do
+    directory global_template['dest'] do
       action :create
       recursive true
     end
