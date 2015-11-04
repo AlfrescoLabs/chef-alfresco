02696fe86924558535084da79cc4bc7b4633afdf
Maurizio (maoo) Pillitu <maurizio.pillitu@alfresco.com>
2015-11-04 16:10:43 +0100

    using s3.bucketLocation prop for endpoint s3 address

--- recipes/_alfrescoproperties-attributes.rb
+++ recipes/_alfrescoproperties-attributes.rb
@@ -31,10 +31,9 @@ rescue
 end
 
 # S3 default values (if not enabled, alfresco will ignore them)
-node.default['alfresco']['properties']['s3.region'] = "us-east-1"
 node.default['alfresco']['properties']['s3.encryption'] = "AES256"
 node.default['alfresco']['properties']['s3.flatRoot'] = true
-node.default['alfresco']['properties']['s3service.s3-endpoint'] = "s3.${s3.region}.amazonaws.com"
+node.default['alfresco']['properties']['s3service.s3-endpoint'] = "s3.${s3.bucketRegion}.amazonaws.com"
 node.default['alfresco']['properties']['s3service.https-only'] = true
 node.default['alfresco']['properties']['s3service.max-thread-count'] = "5"
 
