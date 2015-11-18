62c3805ccb29ef6bb3d47094030a95773580d167
Maurizio (maoo) Pillitu <maurizio.pillitu@alfresco.com>
2015-11-18 10:24:24 +0100

    reverting changes for logrotate

--- attributes/logrotate.rb
+++ attributes/logrotate.rb
@@ -14,13 +14,6 @@ default['logrotate']['global']['/var/log/tomcat-*/localhost.*'] = {
   'rotate' => node['logrotate']['rotate']
 }
 
-default['logrotate']['global']['/var/log/haproxy/haproxy.log'] = {
-  'missingok' => true,
-  'daily' => true,
-  'compress' => true,
-  'rotate' => node['logrotate']['rotate']
-}
-
 # TODO - change log4j config for alfresco.log.<timestamp>, share and solr
 default['logrotate']['global']['/var/log/tomcat-*/*.log.*'] = {
   'missingok' => true,
