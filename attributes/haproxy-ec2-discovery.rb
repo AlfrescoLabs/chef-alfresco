b44fab67901ab323fa48ad9b8145a2f519cc1e6e
Maurizio (maoo) Pillitu <maurizio.pillitu@alfresco.com>
2016-01-05 15:46:22 +0100

    fixed issue on rsyslog config for haproxy

--- attributes/haproxy-ec2-discovery.rb
+++ attributes/haproxy-ec2-discovery.rb
@@ -1,5 +1,6 @@
 default['haproxy']['ec2']['discovery_enabled'] = false
 
+# TODO - are these needed?
 default['haproxy']['ec2']['discovery_chef_erb'] = "/etc/cron.d/haproxy-discovery.cron"
 default['haproxy']['ec2']['discovery_chef_json'] = "/etc/chef/haproxy-discovery.json"
 
