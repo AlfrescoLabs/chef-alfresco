2011d98bb32bd0ec6c95147d63b74a350514b765
Maurizio (maoo) Pillitu <maurizio.pillitu@alfresco.com>
2016-01-29 16:16:02 +0100

    fixed rsyslog file locations

--- attributes/rsyslog.rb
+++ attributes/rsyslog.rb
@@ -23,14 +23,10 @@ default['rsyslog']['file_inputs']['mysql-error']['file'] = '/var/log/mysql-defau
 default['rsyslog']['file_inputs']['mysql-error']['severity'] = 'error'
 default['rsyslog']['file_inputs']['mysql-error']['priority'] = 57
 
-default['rsyslog']['file_inputs']['mysqld']['file'] = '/var/log/mysqld.log'
-default['rsyslog']['file_inputs']['mysqld']['severity'] = 'info'
-default['rsyslog']['file_inputs']['mysqld']['priority'] = 58
-
 default['rsyslog']['file_inputs']['nginx']['file'] = '/var/log/nginx/error.log'
 default['rsyslog']['file_inputs']['nginx']['severity'] = 'error'
 default['rsyslog']['file_inputs']['nginx']['priority'] = 56
 
-default['rsyslog']['file_inputs']['psql-error']['file'] = '/var/log/postgresql/error.log'
-default['rsyslog']['file_inputs']['psql-error']['severity'] = 'error'
-default['rsyslog']['file_inputs']['psql-error']['priority'] = 57
+# default['rsyslog']['file_inputs']['psql-error']['file'] = '/var/log/postgresql/error.log'
+# default['rsyslog']['file_inputs']['psql-error']['severity'] = 'error'
+# default['rsyslog']['file_inputs']['psql-error']['priority'] = 57
