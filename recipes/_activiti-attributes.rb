d8ea4f3d85727be04d71d7819c4c702923de6080
Maurizio (maoo) Pillitu <maurizio.pillitu@alfresco.com>
2016-02-17 16:52:58 +0100

    install activiti war only if component is enabled

--- recipes/_activiti-attributes.rb
+++ recipes/_activiti-attributes.rb
@@ -2,7 +2,6 @@ node.default['activiti']['groupId'] = 'org.activiti'
 node.default['activiti']['artifactId'] = "activiti-webapp-explorer2"
 node.default['activiti']['version'] = '5.14'
 
-node.default['artifacts']['activiti']['enabled'] = true
 node.default['artifacts']['activiti']['groupId'] = node['activiti']['groupId']
 node.default['artifacts']['activiti']['artifactId'] = node['activiti']['artifactId']
 node.default['artifacts']['activiti']['version'] = node['activiti']['version']
--- recipes/activiti.rb
+++ recipes/activiti.rb
@@ -1,23 +1,31 @@
-# create database
-db_database   = node['activiti']['properties']['db.dbname']
-db_host       = node['alfresco']['properties']['db.host']
-db_root_user = node['alfresco']['db']['root_user']
-mysql_root_password = node['alfresco']['db']['server_root_password']
+node.default['artifacts']['activiti']['enabled'] = true
 
-mysql_connection_info = {
-    :host     => db_host,
-    :username => db_root_user,
-    :password => mysql_root_password
-}
+if node['alfresco']['components'].include? 'mysql'
+  # create database
+  db_database   = node['activiti']['properties']['db.dbname']
+  db_host       = node['alfresco']['properties']['db.host']
+  db_root_user = node['alfresco']['db']['root_user']
+  mysql_root_password = node['alfresco']['db']['server_root_password']
 
+  mysql_connection_info = {
+      :host     => db_host,
+      :username => db_root_user,
+      :password => mysql_root_password
+  }
 
-mysql_database db_database do
-  connection mysql_connection_info
-  action :create
+  mysql_database db_database do
+    connection mysql_connection_info
+    action :create
+  end
 end
 
+db_properties_path = "#{node['alfresco']['home']}/activiti/webapps/activiti/WEB-INF/classes/db.properties"
 
-template "#{node['alfresco']['home']}-activiti/webapps/activiti/WEB-INF/classes/db.properties" do
+if node['tomcat']['run_base_instance']
+  db_properties_path = "#{node['alfresco']['home']}/webapps/activiti/WEB-INF/classes/db.properties"
+end
+
+template db_properties_path do
   source 'activiti/db.properties.erb'
   owner node['alfresco']['user']
   mode '0644'
