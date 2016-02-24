76bcfd77b9a7f391bf0c9b591fe14a52383c375a
Sergiu-Alexandru Vidrascu <Sergiu.Vidrascu@ness.com>
2016-02-24 16:57:53 +0200

    if activiti is removed from the default components list it should also be removed from the default services list

--- attributes/default.rb
+++ attributes/default.rb
@@ -126,5 +126,5 @@ default['alfresco']['db']['allowed_host'] = "%"
 
 # Alfresco services configuration
 default["alfresco"]["start_service"] = true
-default['alfresco']['restart_services'] = ['alfresco','share','solr', 'activiti']
+default['alfresco']['restart_services'] = ['alfresco','share','solr']
 default['alfresco']['restart_action']   = :restart
