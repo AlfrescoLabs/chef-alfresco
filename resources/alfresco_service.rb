fa7d870b57e51fa9449c56a0ee04dd601148ce13
Sergiu-Alexandru Vidrascu <Sergiu.Vidrascu@ness.com>
2016-03-15 13:56:18 +0200

    Fix startup issue

--- resources/alfresco_service.rb
+++ resources/alfresco_service.rb
@@ -1,34 +1,23 @@
 resource_name :alfresco_service
 
 property :service_name, String, name_property: true
-property :enabled, [TrueClass, FalseClass], default: false
 property :autorestart, [TrueClass, FalseClass], default: true
 property :autostart, [TrueClass, FalseClass], default: true
 property :user, String, default: nil
 property :directory, String, default: nil
 property :command, String, default: "test"
 property :environment, Hash, default: {}
-property :action, Array, default: [:enable]
 
 default_action :nothing
-
-load_current_value do
-  if enabled
-    action = [:enable , :start]
-  else
-    action = :enable
-  end
-end
-
 
 action :create do
 
   include_recipe 'supervisor::default'
-  r = resources(service: "supervisor")
-  r.action(:nothing)
+  service 'supervisor' do
+    action :nothing
+  end
 
   user new_resource.user
-  action new_resource.action
   command new_resource.command
   autorestart new_resource.autorestart
   autostart new_resource.autostart
@@ -37,7 +26,7 @@ action :create do
 
   supervisor_service "#{service_name}" do
     command new_resource.command
-    action new_resource.action
+    action :enable
     user new_resource.user
     autorestart new_resource.autorestart
     autostart new_resource.autostart
