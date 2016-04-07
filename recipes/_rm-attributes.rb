85ee11bae2b1a28c3834b3dac42b27b0b3e5a8cc
marcomancuso <marco.mancuso@gmail.com>
2016-04-07 12:47:21 +0100

    Use RM 2.4 for Alfresco 5.1

--- recipes/_rm-attributes.rb
+++ recipes/_rm-attributes.rb
@@ -1,13 +1,41 @@
-node.default['artifacts']['rm']['groupId'] = 'org.alfresco'
-node.default['artifacts']['rm']['artifactId'] = 'alfresco-rm'
-node.default['artifacts']['rm']['version'] = '2.3'
-node.default['artifacts']['rm']['destination'] = node['alfresco']['amps_folder']
-node.default['artifacts']['rm']['owner'] = node['alfresco']['user']
-node.default['artifacts']['rm']['type'] = "amp"
+#node.default['artifacts']['rm']['groupId'] = 'org.alfresco'
+#node.default['artifacts']['rm']['artifactId'] = 'alfresco-rm'
+#node.default['artifacts']['rm']['version'] = '2.3'
+#node.default['artifacts']['rm']['destination'] = node['alfresco']['amps_folder']
+#node.default['artifacts']['rm']['owner'] = node['alfresco']['user']
+#node.default['artifacts']['rm']['type'] = "amp"
 
-node.default['artifacts']['rm-share']['groupId'] = 'org.alfresco'
-node.default['artifacts']['rm-share']['artifactId'] = 'alfresco-rm-share'
-node.default['artifacts']['rm-share']['version'] = '2.3'
-node.default['artifacts']['rm-share']['destination'] = node['alfresco']['amps_share_folder']
-node.default['artifacts']['rm-share']['owner'] = node['alfresco']['user']
-node.default['artifacts']['rm-share']['type'] = "amp"
+#node.default['artifacts']['rm-share']['groupId'] = 'org.alfresco'
+#node.default['artifacts']['rm-share']['artifactId'] = 'alfresco-rm-share'
+#node.default['artifacts']['rm-share']['version'] = '2.3'
+#node.default['artifacts']['rm-share']['destination'] = node['alfresco']['amps_share_folder']
+#node.default['artifacts']['rm-share']['owner'] = node['alfresco']['user']
+#node.default['artifacts']['rm-share']['type'] = "amp"
+
+node.default['artifacts']['alfresco-rm-community-repo']['groupId'] = "org.alfresco"
+node.default['artifacts']['alfresco-rm-community-repo']['artifactId'] = "alfresco-rm-community-share"
+node.default['artifacts']['alfresco-rm-community-repo']['version'] = "2.4.a-EA"
+node.default['artifacts']['alfresco-rm-community-repo']['type'] = "amp"
+node.default['artifacts']['alfresco-rm-community-repo']['owner'] = node['alfresco']['user']
+node.default['artifacts']['alfresco-rm-community-repo']['destination'] = node['alfresco']['amps_folder']
+
+node.default['artifacts']['alfresco-rm-community-share']['groupId'] = "org.alfresco"
+node.default['artifacts']['alfresco-rm-community-share']['artifactId'] = "alfresco-rm-community-share"
+node.default['artifacts']['alfresco-rm-community-share']['version'] = "2.4.a-EA"
+node.default['artifacts']['alfresco-rm-community-share']['type'] = "amp"
+node.default['artifacts']['alfresco-rm-community-share']['owner'] = node['alfresco']['user']
+node.default['artifacts']['alfresco-rm-community-share']['destination'] = node['alfresco']['amps_share_folder']
+
+node.default['artifacts']['alfresco-rm-enterprise-repo']['groupId'] = "org.alfresco"
+node.default['artifacts']['alfresco-rm-enterprise-repo']['artifactId'] = "alfresco-rm-enterprise-share"
+node.default['artifacts']['alfresco-rm-enterprise-repo']['version'] = "2.4-EA-1"
+node.default['artifacts']['alfresco-rm-enterprise-repo']['type'] = "amp"
+node.default['artifacts']['alfresco-rm-enterprise-repo']['owner'] = node['alfresco']['user']
+node.default['artifacts']['alfresco-rm-enterprise-repo']['destination'] = node['alfresco']['amps_folder']
+
+node.default['artifacts']['alfresco-rm-enterprise-share']['groupId'] = "org.alfresco"
+node.default['artifacts']['alfresco-rm-enterprise-share']['artifactId'] = "alfresco-rm-enterprise-share"
+node.default['artifacts']['alfresco-rm-enterprise-share']['version'] = "2.4-EA-1"
+node.default['artifacts']['alfresco-rm-enterprise-share']['type'] = "amp"
+node.default['artifacts']['alfresco-rm-enterprise-share']['owner'] = node['alfresco']['user']
+node.default['artifacts']['alfresco-rm-enterprise-share']['destination'] = node['alfresco']['amps_share_folder']
--- recipes/rm.rb
+++ recipes/rm.rb
@@ -1,2 +1,7 @@
-node.default['artifacts']['rm']['enabled'] = true
-node.default['artifacts']['rm-share']['enabled'] = true
+#node.default['artifacts']['rm']['enabled'] = true
+#node.default['artifacts']['rm-share']['enabled'] = true
+
+node.default['artifacts']['alfresco-rm-community-repo']['enabled'] = true
+node.default['artifacts']['alfresco-rm-community-share']['enabled'] = true
+node.default['artifacts']['alfresco-rm-enterprise-repo']['enabled'] = true
+node.default['artifacts']['alfresco-rm-enterprise-share']['enabled'] = true
