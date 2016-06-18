e203e6538b508576d2f1826b07fff78037ae2e19
Enzo Rivello <vincenzo.rivello@gmail.com>
2016-06-18 07:32:54 +0100

    SpaceStore -> SpacesStore

--- recipes/solr-config.rb
+++ recipes/solr-config.rb
@@ -3,7 +3,7 @@ node.default['artifacts']['solrhome']['destination'] = node['alfresco']['propert
 node.default['artifacts']['solr4']['enabled'] = true
 
 if node['alfresco']['generate.solr.core.config'] == true
-  node.default['artifacts']['solrhome']['properties']['archive-SpaceStore/conf/solrcore.properties'] = node['alfresco']['archive-solrproperties']
-  node.default['artifacts']['solrhome']['properties']['workspace-SpaceStore/conf/solrcore.properties'] = node['alfresco']['workspace-solrproperties']
+  node.default['artifacts']['solrhome']['properties']['archive-SpacesStore/conf/solrcore.properties'] = node['alfresco']['archive-solrproperties']
+  node.default['artifacts']['solrhome']['properties']['workspace-SpacesStore/conf/solrcore.properties'] = node['alfresco']['workspace-solrproperties']
   node.default['artifacts']['solrhome']['properties']['log4j-solr.properties'] = node['logging']
 end
--- recipes/solr.rb
+++ recipes/solr.rb
@@ -2,7 +2,7 @@ node.default['artifacts']['solrhome']['enabled'] = true
 node.default['artifacts']['solr4']['enabled'] = true
 
 if node['alfresco']['generate.solr.core.config'] == true
-  node.default['artifacts']['solrhome']['properties']['archive-SpaceStore/conf/solrcore.properties'] = node['alfresco']['archive-solrproperties']
-  node.default['artifacts']['solrhome']['properties']['workspace-SpaceStore/conf/solrcore.properties'] = node['alfresco']['workspace-solrproperties']
+  node.default['artifacts']['solrhome']['properties']['archive-SpacesStore/conf/solrcore.properties'] = node['alfresco']['archive-solrproperties']
+  node.default['artifacts']['solrhome']['properties']['workspace-SpacesStore/conf/solrcore.properties'] = node['alfresco']['workspace-solrproperties']
   node.default['artifacts']['solrhome']['properties']['log4j-solr.properties'] = node['logging']
 end
