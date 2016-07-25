ca60d278e9462a20070d4dfaf5c83e34e3eff249
pmeadows <philip.meadows@alfresco.com>
2016-07-25 10:15:03 +0100

    Updating java version

--- attributes/java.rb
+++ attributes/java.rb
@@ -2,6 +2,6 @@
 default['java']['install_flavor'] = 'oracle'
 default['java']['jdk_version'] = '8'
 default['java']['java_home'] = '/usr/lib/jvm/java'
-default['java']['jdk']['8']['x86_64']['url'] = 'http://download.oracle.com/otn-pub/java/jdk/8u91-b14/jdk-8u91-linux-x64.tar.gz'
-default['java']['jdk']['8']['x86_64']['checksum'] = '3f3d7d0cd70bfe0feab382ed4b0e45c0'
+default['java']['jdk']['8']['x86_64']['url'] = 'http://download.oracle.com/otn-pub/java/jdk/8u101-b13/jdk-8u101-linux-x64.tar.gz'
+default['java']['jdk']['8']['x86_64']['checksum'] = 'a7ab8014716b0dac3adcaf5342167699'
 default['java']['oracle']['accept_oracle_download_terms'] = true
