e4097cb325c233f40b70348aa32ec182dc6afeeb
Enzo Rivello <vincenzo.rivello@gmail.com>
2016-07-27 14:48:10 +0100

    bumping version

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      "Installs Alfresco Community and Enterprise Edition."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url ""
 source_url ""
-version "0.6.29"
+version "0.6.44"
 
 # Resolved by Berkshelf, not present in Supermarket or forked
 depends "file"
