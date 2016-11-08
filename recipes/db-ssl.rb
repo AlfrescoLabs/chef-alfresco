17e38e3984efc093b2d83a62dfb68ce4d33d273d
Enzo Rivello <vincenzo.rivello@gmail.com>
2016-11-08 14:22:42 +0000

    adding abdul improved db-ssl

--- recipes/db-ssl.rb
+++ recipes/db-ssl.rb
@@ -1,31 +1,35 @@
-remote_file '/tmp/rds-combined-ca-bundle.pem' do
+remote_file "#{Chef::Config[:file_cache_path]}/rds-combined-ca-bundle.pem" do
   source 'http://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem'
   owner 'root'
   group 'root'
-  mode '0755'  
+  mode '0755'
   action :create_if_missing
 end
 
-script 'split_certs' do
-  interpreter "bash"
-  code <<-EOH
-    cd /tmp
+execute 'split_certs' do
+  command <<-EOF
+    cd #{Chef::Config[:file_cache_path]}
     csplit -sz rds-combined-ca-bundle.pem '/-BEGIN CERTIFICATE-/' '{*}'
-    EOH
-  only_if { ::File.exists?('/tmp/rds-combined-ca-bundle.pem') }
+    EOF
+  only_if { ::File.exists?("#{Chef::Config[:file_cache_path]}/rds-combined-ca-bundle.pem") }
+  not_if "ls -l #{Chef::Config[:file_cache_path]}/xx*"
 end
 
 truststore = node['alfresco']['truststore_file']
 truststore_pass = node['alfresco']['truststore_password']
 truststore_type = node['alfresco']['truststore_type']
 
-Dir["/tmp/xx*"].each do |cert|
+Dir.glob("#{Chef::Config[:file_cache_path]}/xx*").each do |cert|
   execute "import #{cert} to RDS keystore" do
     command <<-EOF
       ALIAS=$(openssl x509 -noout -text -in #{cert} | perl -ne 'next unless /Subject:/; s/.*CN=//; print')
       keytool -import -keystore #{truststore} -storepass #{truststore_pass} -storetype #{truststore_type} -noprompt -alias "$ALIAS" -file #{cert}
       EOF
-    not_if "keytool -list -keystore #{truststore} -storepass #{truststore_pass} -alias #{cert}" 
+    only_if { ::File.exists?(truststore) }
+    not_if do
+      "keytool -list -keystore #{truststore} -storepass #{truststore_pass} -storetype #{truststore_type} -noprompt \
+       -alias \"$(openssl x509 -noout -text -in #{cert} | perl -ne 'next unless /Subject:/; s/.*CN=//; print')\""
+    end
   end
 end
 
