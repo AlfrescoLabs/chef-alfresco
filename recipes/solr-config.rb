5421c6a30cf3fea4e68d29124a5ca219ffb3e033
Enzo Rivello <vincenzo.rivello@gmail.com>
2016-11-11 11:47:16 +0000

    removed solr-config for now

--- recipes/solr-config.rb
+++
@@ -1,9 +0,0 @@
-node.default['artifacts']['solrhome']['enabled'] = true
-node.default['artifacts']['solrhome']['destination'] = node['alfresco']['properties']['dir.root']
-node.default['artifacts']['solr4']['enabled'] = true
-
-if node['alfresco']['generate.solr.core.config'] == true
-  node.default['artifacts']['solrhome']['properties']['archive-SpacesStore/conf/solrcore.properties'] = node['alfresco']['archive-solrproperties']
-  node.default['artifacts']['solrhome']['properties']['workspace-SpacesStore/conf/solrcore.properties'] = node['alfresco']['workspace-solrproperties']
-  node.default['artifacts']['solrhome']['properties']['log4j-solr.properties'] = node['logging']
-end
