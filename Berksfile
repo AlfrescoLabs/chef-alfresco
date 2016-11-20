43f146946f9acba243f9f9b18503fa28627af76d
Enzo Rivello <vincenzo.rivello@gmail.com>
2016-11-20 16:07:50 +0000

    moved alfresco-webserver in the chef-supermarket, removed from berksfile and added only nginx-hardening

--- Berksfile
+++ Berksfile
@@ -8,8 +8,6 @@ cookbook 'tomcat', git:'https://github.com/maoo/tomcat.git', tag: "v11.17.3"
 # Resolve transitive deps of artifact-deployer
 cookbook 'maven', git:'https://github.com/maoo/maven.git', tag: "v1.2.0-fork"
 cookbook 'file', git: 'https://github.com/jenssegers/chef-file.git', tag: "v1.0.0"
-cookbook 'nginx-hardening', git: 'https://github.com/dev-sec/chef-nginx-hardening'
 cookbook 'commons', git: 'https://github.com/Alfresco/chef-commons.git'
 cookbook 'database', git: 'https://github.com/enzor/database.git'
-cookbook 'alfresco-webserver', git: 'https://github.com/Alfresco/chef-alfresco-webserver', tag: 'v0.7.1'
-# cookbook 'commons', path: '../chef-commons'
+cookbook 'nginx-hardening', git: 'https://github.com/dev-sec/chef-nginx-hardening', branch: 'master'
--- metadata.rb
+++ metadata.rb
@@ -1,37 +1,38 @@
-name             "alfresco"
-maintainer       "Alfresco T&A"
-maintainer_email "enzo.rivello@alfresco.com"
-license          "Apache 2.0"
-description      "Installs Alfresco Community and Enterprise Edition."
+name             'alfresco'
+maintainer       'Alfresco T&A'
+maintainer_email 'enzo.rivello@alfresco.com'
+license          'Apache 2.0'
+description      'Installs Alfresco Community and Enterprise Edition.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-issues_url ""
-source_url ""
-version "0.7.11"
+issues_url ''
+source_url ''
+version '0.7.11'
 
 # Resolved by Berkshelf, not present in Supermarket or forked
-depends "file"
-depends "tomcat"
-depends "maven"
-depends "commons"
-depends 'alfresco-webserver'
+depends 'file'
+depends 'tomcat'
+depends 'maven'
+depends 'commons'
+depends 'alfresco-webserver', '~> 0.7.2'
+depends 'nginx-hardening'
 
 # Community cookbooks
-depends "activemq", ">= 1.3.3"
-depends "artifact-deployer", ">= 0.8.18"
-depends 'build-essential', ">= 2.2.3"
-depends "database", ">= 4.0.6"
-depends "ffmpeg", ">= 0.4.4"
-depends "haproxy", "~> 1.6.6"
-depends "imagemagick", ">= 0.2.3"
-depends "java", ">= 1.31.0"
-depends "libreoffice", ">= 0.0.2"
+depends 'activemq', '>= 1.3.3'
+depends 'artifact-deployer', '>= 0.8.18'
+depends 'build-essential', '>= 2.2.3'
+depends 'database', '>= 4.0.6'
+depends 'ffmpeg', '>= 0.4.4'
+depends 'haproxy', '~> 1.6.6'
+depends 'imagemagick', '>= 0.2.3'
+depends 'java', '>= 1.31.0'
+depends 'libreoffice', '>= 0.0.2'
 depends 'line', '>= 0.6.2'
-depends "logstash-forwarder", '>= 0.1.1'
+depends 'logstash-forwarder', '>= 0.1.1'
 depends 'mysql', '~> 7.2'
-depends 'mysql2_chef_gem', ">= 1.0.1"
-depends "openssl", ">= 4.0.0"
-depends "postgresql", ">= 3.4.18"
-depends 'rsyslog', ">= 1.15.0"
-depends "swftools", ">= 0.2.4"
+depends 'mysql2_chef_gem', '>= 1.0.1'
+depends 'openssl', '>= 4.0.0'
+depends 'postgresql', '>= 3.4.18'
+depends 'rsyslog', '>= 1.15.0'
+depends 'swftools', '>= 0.2.4'
 depends 'yum-epel'
-depends 'yum-atrpms', ">= 0.1.0"
+depends 'yum-atrpms', '>= 0.1.0'
