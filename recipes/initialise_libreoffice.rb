8ec424bfb523a383fdf4e7307bfd671997fb8670
Marco Mancuso <marco.mancuso@alfresco.com>
2016-11-24 08:24:35 +0000

    ammending notifies command

--- recipes/initialise_libreoffice.rb
+++ recipes/initialise_libreoffice.rb
@@ -4,10 +4,11 @@ execute 'start_libreoffice' do
   group node['libreoffice']['user']
   returns 81
   not_if { ::File.directory?(node['libreoffice']['command']['user_installation_path']) }
-  notifies :delete, directory["#{node['libreoffice']['command']['user_installation_path']}"]
+  notifies :delete, 'directory[user_installation_path]'
 end
 
-directory "#{node['libreoffice']['command']['user_installation_path']}" do
+directory "user_installation_path" do
+  path node['libreoffice']['command']['user_installation_path']
   recursive true
   action :nothing
   user node['libreoffice']['user']
