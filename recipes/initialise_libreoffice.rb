cc6f808db58fda7e742f28a2ab2918df7d383082
Marco Mancuso <marco.mancuso@alfresco.com>
2016-11-25 18:00:11 +0000

    one line command

--- recipes/initialise_libreoffice.rb
+++ recipes/initialise_libreoffice.rb
@@ -1,8 +1,5 @@
 execute 'initialise_libreoffice' do
-  command <<-EOF
-  (#{node['alfresco']['properties']['jodconverter.officeHome']}program/soffice.bin #{node['libreoffice']['command']['full']}
-  rm -rf #{node['libreoffice']['command']['user_installation_path']}) &
-  EOF
+  command "(#{node['alfresco']['properties']['jodconverter.officeHome']}program/soffice.bin #{node['libreoffice']['command']['full']};rm -rf #{node['libreoffice']['command']['user_installation_path']}) &"
   user node['libreoffice']['user']
   group node['libreoffice']['user']
   not_if { ::File.directory?(node['libreoffice']['command']['user_installation_path']) }
