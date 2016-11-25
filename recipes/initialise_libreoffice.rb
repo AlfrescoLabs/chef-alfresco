a943fba56ee41a2044d4219c6cda04b39cbff855
Marco Mancuso <marco.mancuso@alfresco.com>
2016-11-25 21:31:07 +0000

    back to serial process

--- recipes/initialise_libreoffice.rb
+++ recipes/initialise_libreoffice.rb
@@ -1,6 +1,16 @@
-execute 'initialise_libreoffice' do
-  command "(#{node['alfresco']['properties']['jodconverter.officeHome']}program/soffice.bin #{node['libreoffice']['command']['full']};rm -rf #{node['libreoffice']['command']['user_installation_path']}) &"
+execute 'start_libreoffice' do
+  command "#{node['alfresco']['properties']['jodconverter.officeHome']}program/soffice.bin #{node['libreoffice']['command']['full']}"
   user node['libreoffice']['user']
   group node['libreoffice']['user']
+  returns 81
   not_if { ::File.directory?(node['libreoffice']['command']['user_installation_path']) }
+  notifies :delete, 'directory[user_installation_path]', :immediately
+end
+
+directory "user_installation_path" do
+  path node['libreoffice']['command']['user_installation_path']
+  recursive true
+  action :nothing
+  user node['libreoffice']['user']
+  group node['libreoffice']['user']
 end
