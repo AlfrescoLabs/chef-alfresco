a938a44834e37720996b3cfb1f07c410d6793649
Marco Mancuso <marco.mancuso@alfresco.com>
2016-11-29 18:01:12 +0000

    libreoffice should be owned by tomcat

--- test/integration/community/serverspec/components/transformations_spec.rb
+++ test/integration/community/serverspec/components/transformations_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "LibreOffice installation" do
   describe file("/opt/libreoffice4.4/") do
     it { should exist }
-    it { should be_owned_by 'root' }
+    it { should be_owned_by 'tomcat' }
   end
 
   describe command("cat /usr/share/tomcat/shared/classes/alfresco-global.properties") do
