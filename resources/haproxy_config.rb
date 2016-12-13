f746788cd224d1001ec2807b4dfdf285f246a893
Abdul Mohammed <abdul.mohammed@alfresco.com>
2016-12-13 16:17:30 +0000

    JIRA:redd-692 Undo commit

--- resources/haproxy_config.rb
+++ resources/haproxy_config.rb
@@ -102,7 +102,7 @@ action :run do
   template '/etc/haproxy/haproxy.cfg' do
     source node['haproxy']['conf_template_source']
     cookbook node['haproxy']['conf_cookbook']
-    variables ({:haproxy_backends => haproxy_backends.uniq })
+    variables ({:haproxy_backends => haproxy_backends })
     notifies :restart, 'service[haproxy]', :delayed
   end
 
