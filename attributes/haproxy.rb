62982410dd09a0faddf6b9c51b27b44753e0852d
Abdul Mohammed <abdul.mohammed@alfresco.com>
2016-12-19 15:53:06 +0000

    JIRA:redd-692 Haproxy configuration re-org

--- attributes/haproxy.rb
+++ attributes/haproxy.rb
@@ -22,8 +22,8 @@ default['haproxy']['stats_auth'] = "admin"
 default['haproxy']['stats_pwd'] = "changeme"
 
 default['haproxy']['log_level'] = "info"
-default['haproxy']['redirect']['http_https']['enabled'] = true
-default['haproxy']['enable_ssl_header'] = true
+default['haproxy']['redirect']['http_https']['enabled'] = false
+default['haproxy']['enable_ssl_header'] = false
 default['haproxy']['ssl_header'] = "http-response set-header Strict-Transport-Security max-age=15768000;\\ includeSubDomains;\\ preload;"
 
 #default['haproxy']['logging'] = "option httplog"
