30639ff54e7261ec0ec738ab050b1f84ee494872
Marco Mancuso <marco.mancuso@alfresco.com>
2017-02-20 15:16:49 -0500

    adding log4j

--- recipes/redeploy.rb
+++ recipes/redeploy.rb
@@ -148,7 +148,8 @@ if node['alfresco']['components'].include?('solr6')
                   "#{solr_home}/alfresco/conf/solrcore.properties",
                   "#{solr_home}/archive/conf/solrcore.properties",
                   "#{solr_home}/templates/rerank/conf/solrcore.properties",
-                  "#{node['solr6']['solr_env_dir']}/solr.in.sh"
+                  "#{node['solr6']['solr_env_dir']}/solr.in.sh",
+                  "#{node['solr6']['solr-in-sh']['SOLR_PID_DIR']/solrcore.properties}"
   ]
 
   # replacing configuration files
