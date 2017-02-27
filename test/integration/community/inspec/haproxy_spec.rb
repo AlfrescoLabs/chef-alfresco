451c0a5ead75621019e5a9b1ec3d860813954d02
Abdul Mohammed <abdul.mohammed@alfresco.com>
2017-02-27 11:27:11 +0000

    JIRA:redd-775-revert All changed made for redd-775 branch and its inspec tests of develop branch

--- test/integration/community/inspec/haproxy_spec.rb
+++ test/integration/community/inspec/haproxy_spec.rb
@@ -6,7 +6,5 @@ control "alfresco-04" do
     it { should exist }
     it { should be_file }
     its('content') { should match 'http-response set-header Strict-Transport-Security' }
-    its('content') { should match 'email_systeminfo path_reg' }
-    its('content') { should match 'http-request deny if email_systeminfo' }
   end
 end
