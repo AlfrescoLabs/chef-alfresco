6f747130678276383a11310d459662c9890efedf
juena <jferdous@MBP512-JFERDOUS-0217.local>
2017-03-16 16:49:23 +0000

    InSpec tests for the recipes repo, share and solr added for the suite community sol6 and enterprise sol6

---
+++ test/integration/community-solr6-52/inspec/share_spec.rb
@@ -0,0 +1,42 @@
+control 'alfresco-06' do
+  impact 0.7
+  title 'Checks for the Share'
+
+  describe directory('/usr/share/tomcat/shared/classes/alfresco/web-extension') do
+    it { should exist }
+    it { should be_directory }
+    its('owner') { should cmp 'tomcat' }
+    its('group') { should cmp 'tomcat' } 
+    it { should be_readable.by_user('tomcat') }
+    it { should be_writable.by_user('tomcat') }
+    it { should be_executable.by_user('tomcat') }
+    it { should be_readable.by_user('nginx') }
+    it { should be_executable.by_user('nginx') }
+    it { should_not be_writable.by_user('nginx') }
+  end
+
+  describe file('/usr/share/tomcat/shared/classes/alfresco/web-extension/share-config-custom.xml') do
+    it { should exist }
+    it { should be_file }
+    its('owner') { should cmp 'tomcat' }
+    its('group') { should cmp 'tomcat' } 
+    it { should be_readable.by_user('tomcat') }
+    it { should be_writable.by_user('tomcat') }
+    it { should_not be_executable.by_user('tomcat') }
+    it { should be_readable.by_user('nginx') }
+    it { should_not be_executable.by_user('nginx') }
+    it { should_not be_writable.by_user('nginx') }
+  end
+  describe file('/usr/share/tomcat/shared/classes/alfresco/web-extension/share-cluster-application-context.xml') do
+    it { should exist }
+    it { should be_file }
+    its('owner') { should cmp 'tomcat' }
+    its('group') { should cmp 'tomcat' } 
+    it { should be_readable.by_user('tomcat') }
+    it { should be_writable.by_user('tomcat') }
+    it { should_not be_executable.by_user('tomcat') }
+    it { should be_readable.by_user('nginx') }
+    it { should_not be_executable.by_user('nginx') }
+    it { should_not be_writable.by_user('nginx') }
+  end
+end
