cb0cff8d8a04947a15eb106f12c53afaffaa0343
Abdul Mohammed <abdul.mohammed@alfresco.com>
2017-04-21 12:36:29 +0100

    Replace JCEKS with JKS and use cacert default

--- recipes/db-ssl.rb
+++ recipes/db-ssl.rb
@@ -14,18 +14,20 @@ execute 'split_certs' do
   only_if { ::File.exist?("#{Chef::Config[:file_cache_path]}/rds-combined-ca-bundle.pem") }
 end
 
-truststore = node['alfresco']['truststore_file']
-truststore_pass = node['alfresco']['truststore_password']
-truststore_type = node['alfresco']['truststore_type']
+truststore = "#{node['java']['java_home']}/jre/lib/security/cacerts"
+truststore_pass = 'changeit'
 
 ruby_block 'Import AWS RDS Certs' do
   block do
     Dir.glob("#{Chef::Config[:file_cache_path]}/xx*").each do |cert|
       Mixlib::ShellOut.new(
-        %[ keytool -import -keystore #{truststore} -storepass #{truststore_pass} -storetype #{truststore_type} -noprompt \
+        %[ keytool -import -keystore #{truststore} -storepass #{truststore_pass} -noprompt \
         -alias \"$(openssl x509 -noout -text -in #{cert} | perl -ne 'next unless /Subject:/; s/.*CN=//; print')\" -file #{cert} ]
       ).run_command
     end
   end
   action :run
 end
+
+ssl_db_conf = " -Djavax.net.ssl.trustStore=#{truststore} -Djavax.net.ssl.trustStorePassword=#{truststore_pass}"
+node.default['alfresco']['repo_tomcat_instance']['java_options']['others'] = "#{node['alfresco']['repo_tomcat_instance']['java_options']['others']} #{ssl_db_conf}"
