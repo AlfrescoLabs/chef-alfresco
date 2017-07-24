d55e7926f8a48a307f338d8f9783ce04adc20d3d
Martin Whittington <martin.whittington@alfresco.com>
2017-07-24 14:17:34 +0100

    DEVOPS-8979 - Update to Java 8u141

--- attributes/java.rb
+++ attributes/java.rb
@@ -2,8 +2,8 @@
 default['java']['install_flavor'] = 'oracle'
 default['java']['jdk_version'] = '8'
 default['java']['java_home'] = '/usr/lib/jvm/java'
-default['java']['jdk']['8']['x86_64']['url'] = 'http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz'
-default['java']['jdk']['8']['x86_64']['checksum'] = '75b2cb2249710d822a60f83e28860053'
+default['java']['jdk']['8']['x86_64']['url'] = 'http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.tar.gz'
+default['java']['jdk']['8']['x86_64']['checksum'] = '8cf4c4e00744bfafc023d770cb65328c'
 default['java']['oracle']['accept_oracle_download_terms'] = true
 
 # Java CA Certstore
