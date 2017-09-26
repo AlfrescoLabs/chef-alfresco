2dc9d58032b3a691b04ca0a17bb4fed4ec7e9d36
Abdul Mohammed <abdul.mohammed@alfresco.com>
2017-09-26 20:03:55 +0100

    Issue #257: Fixed ImageMagick version logic of installation in transformation.rb recipe

--- attributes/default.rb
+++ attributes/default.rb
@@ -80,7 +80,7 @@ default['artifact-deployer']['maven']['repositories']['public']['url'] = 'https:
 
 # 3rd-party defaults
 default['alfresco']['imagemagick_version'] = '6.9.5-9'
-default['alfresco']['use_imagemagick_os_repo'] = true
+default['alfresco']['use_imagemagick_os_repo'] = false
 default['alfresco']['imagemagick_libs_name'] = "ImageMagick-libs-#{node['alfresco']['imagemagick_version']}.x86_64.rpm"
 default['alfresco']['imagemagick_libs_url'] = "ftp://ftp.icm.edu.pl/vol/rzm4/ImageMagick/linux/CentOS/x86_64/#{node['alfresco']['imagemagick_libs_name']}"
 default['alfresco']['imagemagick_name'] = "ImageMagick-#{node['alfresco']['imagemagick_version']}.x86_64.rpm"
