9ef0321f952fd0a10f75909bf3329de3373ae292
Abdul Mohammed <abdul.mohammed@alfresco.com>
2018-01-17 16:02:57 +0000

    Bump Java to JDK 8u162 which contains security updates from Oracle for January 2018

--- attributes/java.rb
+++ attributes/java.rb
@@ -2,7 +2,7 @@
 default['java']['install_flavor'] = 'oracle'
 default['java']['jdk_version'] = '8'
 default['java']['java_home'] = '/usr/lib/jvm/java'
-default['java']['jdk']['8']['x86_64']['url'] = 'http://download.oracle.com/otn-pub/java/jdk/8u162-b12/0da788060d494f5095bf8624735fa2f1/jdk-8u162-linux-x64.rpm'
+default['java']['jdk']['8']['x86_64']['url'] = 'http://download.oracle.com/otn-pub/java/jdk/8u162-b12/0da788060d494f5095bf8624735fa2f1/jdk-8u162-linux-x64.tar.gz'
 default['java']['jdk']['8']['x86_64']['checksum'] = '781e3779f0c134fb548bde8b8e715e90'
 default['java']['oracle']['accept_oracle_download_terms'] = true
 
